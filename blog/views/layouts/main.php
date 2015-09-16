ccbb257a05a0009448fe44985c078f5c14d31c0e
vincent <apanly@163.com>
2015-09-17 00:43:01 +0800

    vincent  -- 加入关键字统计

--- blog/views/layouts/main.php
+++ blog/views/layouts/main.php
@@ -10,11 +10,9 @@ AppAsset::register($this);
 <head>
     <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-
     <title><?= Html::encode($this->title) ?></title>
-    <meta name="description"
-          content="Ghost 是一套基于 Node.js 构建的开源博客平台（Open source blogging platform），具有易用的书写界面和体验，博客内容默认采用 Markdown 语法书写，目标是取代臃肿的 Wordpress。"/>
-    <meta name="keywords" content="Ghost,blog,Ghost中国,Ghost博客,Ghost中文,Ghost中文文档">
+    <meta name="description" content="郭大帅哥的梦平台，一个有梦的php开发工程师"/>
+    <meta name="keywords" content="郭大帅哥的梦平台，一个有梦的php开发工程师">
 
     <meta name="HandheldFriendly" content="True"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
