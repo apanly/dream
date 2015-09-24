7391975f6e543df86682f4a1d2033b1f5d8eb587
vincent <apanly@163.com>
2015-09-24 17:47:23 +0800

    vincent -- 状态不对

--- common/service/SpiderService.php
+++ common/service/SpiderService.php
@@ -22,7 +22,7 @@ class SpiderService {
         $model_queue = new SpiderQueue();
         $model_queue->url = $url;
         $model_queue->hash_url = md5($url);
-        $model_queue->status = -1 ;
+        $model_queue->status = -2 ;
         $model_queue->updated_time = $date_now;
         $model_queue->created_time = $date_now;
         $model_queue->save(0);
