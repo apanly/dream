811152c26de771365bef4d6793a6ad68f221d5de
vincent <apanly@163.com>
2015-10-04 00:43:16 +0800

    vincent -- 在服务器上看看

--- environments/prod/pic1/index.php
+++ environments/prod/pic1/index.php
@@ -1,5 +1,5 @@
 <?php
-
+//var_dump($_SERVER);exit();
 function notFound(){
     header("HTTP/1.1 404 Not Found");
     exit;
@@ -23,6 +23,21 @@ function resizeimage($filename,$w,$h = 0,$format = "jpg"){
 //        header('Etag:'.$etag);
 //    };
 
+    $modified_time = $_SERVER['HTTP_IF_MODIFIED_SINCE'];
+    //定义一个合理缓存时间。合理值屈居于页面本身、访问者的数量和页面的更新频率，此处为3600秒(1小时)。
+    $time = 30 * 24 * 60 * 60;
+    if (strtotime($modified_time) + $time > time()) {
+        header("http/1.1 304");
+        exit();
+    }
+
+    //发送Last-Modified头标，设置文档的最后的更新日期。
+    header ("Last-Modified: " .gmdate("D, d M Y H:i:s", time() )." GMT");
+    //发送Expires头标，设置当前缓存的文档过期时间，GMT格式。
+    header ("Expires: " .gmdate("D, d M Y H:i:s", time()+$time )." GMT");
+    //发送Cache_Control头标，设置xx秒以后文档过时,可以代替Expires，如果同时出现，max-age优先。
+    header ("Cache-Control: max-age=$time");
+
     $path = "/data/www/pic1{$filename}";
 
     list($o_w, $o_h) = getimagesize($path);
@@ -40,14 +55,7 @@ function resizeimage($filename,$w,$h = 0,$format = "jpg"){
     $src=imagecreatefromjpeg($path);
     $image=imagecreatetruecolor($w, $h);
     imagecopyresampled($image, $src, 0, 0, 0, 0, $w, $h, $o_w, $o_h);
-    //定义一个合理缓存时间。合理值屈居于页面本身、访问者的数量和页面的更新频率，此处为3600秒(1小时)。
-    $time = 30 * 24 * 60 * 60;
-    //发送Last-Modified头标，设置文档的最后的更新日期。
-    header ("Last-Modified: " .gmdate("D, d M Y H:i:s", time() )." GMT");
-    //发送Expires头标，设置当前缓存的文档过期时间，GMT格式。
-    header ("Expires: " .gmdate("D, d M Y H:i:s", time()+$time )." GMT");
-    //发送Cache_Control头标，设置xx秒以后文档过时,可以代替Expires，如果同时出现，max-age优先。
-    header ("Cache-Control: max-age=$time");
+
     header('content-type:image/jpg');
     imagejpeg($image,null,100);
     imagedestroy($image);
