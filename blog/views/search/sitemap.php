d31aaf0b8e70607b466a5159a4d8efd2656bc4b0
vincent <apanly@163.com>
2015-10-26 16:37:52 +0800

    vincent -- 标题提示

--- blog/views/search/sitemap.php
+++ blog/views/search/sitemap.php
@@ -1,3 +1,7 @@
+<?php
+    $xml_header = '<?xml version="1.0" encoding="utf-8"?>';
+?>
+<?=$xml_header;?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 <?php if($data):?>
 <?php foreach($data as $_item):?>
