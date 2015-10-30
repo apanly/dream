4581b1fac0a421a43e79a203507b1504c45b994a
vincent <apanly@163.com>
2015-10-30 23:09:52 +0800

    vincent -- 移动版

--- common/config/params.php
+++ common/config/params.php
@@ -4,6 +4,6 @@ return [
         'description' => '郭大帅哥的梦博客，一个有梦的php开发工程师',
         'keywords' => '郭大帅哥的梦博客，一个有梦的php开发工程师'
     ],
-    'Copyright' => "<a href='/wap'>移动版</a>
+    'Copyright' => "<a href='http://m.vincentguo.cn'>移动版</a>
     <br/>Copyright © 2015.郭大帅哥 All rights reserved | PHP，Python 工程师"
 ];
