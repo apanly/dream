1ecb423ecc5a58d50fe0487f69b7cfb6ec64a66b
vincent <apanly@163.com>
2015-11-11 22:48:46 +0800

    vincent -- 看看美女

--- blog/modules/game/controllers/MvController.php
+++ blog/modules/game/controllers/MvController.php
@@ -3,12 +3,40 @@
 namespace blog\modules\game\controllers;
 
 use blog\modules\game\controllers\common\BaseController;
+use common\components\DataHelper;
+use common\models\games\Doubanmz;
 
 class MvController extends BaseController
 {
     public function actionIndex(){
+        $p = intval( $this->get("p",1) );
+        if(!$p){
+            $p = 1;
+        }
+
+
+        $pagesize = 10;
+        $query = Doubanmz::find();
+        $total_count = $query->count();
+        $offset = ($p - 1) * $pagesize;
+        $mz_list = $query->orderBy("id desc")
+            ->offset($offset)
+            ->limit($pagesize)
+            ->all();
+
+        $data = [];
+        if( $mz_list ){
+            foreach( $mz_list as $_mz_info ){
+                $data[] = [
+                    "id" => $_mz_info["id"],
+                    "title" => mb_substr( DataHelper::encode( $_mz_info["title"] ),0,10,"utf-8"),
+                    "src_url" => $_mz_info['src_url']
+                ];
+            }
+        }
+
         return $this->render("index",[
-            "auth" => $this->getCookie($this->auth_cookie_name)
+            "list" => $data
         ]);
     }
 } 
--- blog/modules/game/views/mv/index.php
+++ blog/modules/game/views/mv/index.php
@@ -6,11 +6,19 @@
         </div>
     </div>
     <div class="am-u-lg-12 am-u-md-12 am-u-sm-12">
-        <div class="am-slider am-slider-default" data-am-flexslider id="demo-slider-0">
-            <ul class="am-slides">
-                <li><img src="http://s.amazeui.org/media/i/demos/bing-1.jpg" /></li>
-                <li><img src="http://s.amazeui.org/media/i/demos/bing-2.jpg" /></li>
-            </ul>
-        </div>
+        <?php if($list):?>
+        <ul data-am-widget="gallery" class="am-u-lg-12 am-u-md-12 am-u-sm-12 am-gallery-bordered" >
+            <?php foreach($list as $_item):?>
+            <li>
+                <div class="am-gallery-item">
+                    <a href="javascript:void(0);">
+                        <img src="<?=$_item['src_url'];?>"  alt="<?=$_item['title'];?>"/>
+                        <h3 class="am-gallery-title"><?=$_item['title'];?></h3>
+                    </a>
+                </div>
+            </li>
+            <?php endforeach;?>
+        </ul>
+        <?php endif;?>
     </div>
 </div>
