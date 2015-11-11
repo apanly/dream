c946f5116d6e46a2a54957cdd05ab9bf6c3a7571
vincent <apanly@163.com>
2015-11-11 23:07:40 +0800

    vincent -- ok了

--- blog/modules/game/controllers/MvController.php
+++ blog/modules/game/controllers/MvController.php
@@ -17,7 +17,10 @@ class MvController extends BaseController
 
         $pagesize = 10;
         $query = Doubanmz::find();
+
         $total_count = $query->count();
+        $total_page = ceil($total_count/$pagesize);
+
         $offset = ($p - 1) * $pagesize;
         $mz_list = $query->orderBy("id desc")
             ->offset($offset)
@@ -29,14 +32,29 @@ class MvController extends BaseController
             foreach( $mz_list as $_mz_info ){
                 $data[] = [
                     "id" => $_mz_info["id"],
-                    "title" => mb_substr( DataHelper::encode( $_mz_info["title"] ),0,10,"utf-8"),
+                    "title" => DataHelper::encode( $_mz_info["title"] ),
                     "src_url" => $_mz_info['src_url']
                 ];
             }
         }
 
+        $has_pre = $has_next = false;
+
+        if( ( count( $data ) >= $pagesize ) && ( $p <= $total_page ) ){
+            $has_next = true;
+        }
+
+        if( $p > 1 ){
+            $has_pre = true;
+        }
+
         return $this->render("index",[
-            "list" => $data
+            "list" => $data,
+            "p" => $p,
+            "urls" => [
+                "has_pre" => $has_pre,
+                "has_next" => $has_next
+            ]
         ]);
     }
 } 
--- blog/modules/game/views/mv/index.php
+++ blog/modules/game/views/mv/index.php
@@ -1,8 +1,21 @@
+<?php
+use \blog\components\UrlService;
+?>
 <div class="am-g">
     <div class="am-u-lg-12 am-u-md-12 am-u-sm-12">
-        <div class="am-cf">
-            <button type="button" class="am-btn am-btn-primary am-fl">上一组</button>
-            <button type="button" class="am-btn am-btn-danger am-fr">下一组</button>
+        <div class="am-btn-toolbar"  style="padding: 0 16px;">
+            <?php if( $urls['has_pre'] ):?>
+                <a href="<?=UrlService::buildGameUrl("/mv/index",['p' => ($p-1)]);?>" class="am-btn am-btn-warning am-fl">上一组</a>
+            <?php else:?>
+                <a href="javascript:void(0);" class="am-btn am-btn-default am-fl">上一一组</a>
+            <?php endif;?>
+
+            <?php if( $urls['has_next'] ):?>
+                <a href="<?=UrlService::buildGameUrl("/mv/index",['p' => ($p+1)]);?>" class="am-btn am-btn-warning am-fr">下一组</a>
+            <?php else:?>
+                <a href="javascript:void(0);" class="am-btn am-btn-default am-fr">下一组</a>
+            <?php endif;?>
+
         </div>
     </div>
     <div class="am-u-lg-12 am-u-md-12 am-u-sm-12">
@@ -20,5 +33,20 @@
             <?php endforeach;?>
         </ul>
         <?php endif;?>
+    </div>
+    <div class="am-u-lg-12 am-u-md-12 am-u-sm-12">
+        <div class="am-btn-toolbar"  style="padding: 0 16px;">
+            <?php if( $urls['has_pre'] ):?>
+                <a href="<?=UrlService::buildGameUrl("/mv/index",['p' => ($p-1)]);?>" class="am-btn am-btn-warning am-fl">上一组</a>
+            <?php else:?>
+                <a href="javascript:void(0);" class="am-btn am-btn-default am-fl">上一一组</a>
+            <?php endif;?>
+
+            <?php if( $urls['has_next'] ):?>
+                <a href="<?=UrlService::buildGameUrl("/mv/index",['p' => ($p+1)]);?>" class="am-btn am-btn-warning am-fr">下一组</a>
+            <?php else:?>
+                <a href="javascript:void(0);" class="am-btn am-btn-default am-fr">下一组</a>
+            <?php endif;?>
+        </div>
     </div>
 </div>
