f585f1cd4d9dded037ab99598eb5639c022ee70c
vincent <apanly@163.com>
2015-11-12 22:49:47 +0800

    vincent -- 先把ameiz都是用cdn

--- blog/assets/GameAsset.php
+++ blog/assets/GameAsset.php
@@ -25,39 +25,20 @@ class GameAsset extends AssetBundle
 
     ];
     public function registerAssetFiles($view){
-
-
-//        $this->css = [
-//            GlobalUrlService::buildStaticUrl("/amazeui/css/amazeui.css"),
-//            GlobalUrlService::buildStaticUrl("/amazeui/css/amazeui.min.css"),
-//            'css/wap/common.css',
-//        ];
-//        $this->js = [
-//            'js/jquery/jquery.min.js',
-//            GlobalUrlService::buildStaticUrl("/amazeui/js/amazeui.js"),
-//            GlobalUrlService::buildStaticUrl("/amazeui/js/amazeui.min.js"),
-//            GlobalUrlService::buildStaticUrl("/amazeui/js/amazeui.ie8polyfill.js"),
-//            GlobalUrlService::buildStaticUrl("/amazeui/js/amazeui.ie8polyfill.min.js"),
-//            GlobalUrlService::buildStaticUrl("/amazeui/js/amazeui.widgets.helper.js"),
-//            GlobalUrlService::buildStaticUrl("/amazeui/js/amazeui.widgets.helper.min.js"),
-//            'js/wap/common.js'
-//        ];
-//        parent::registerAssetFiles($view);
-
         $this->css = [
-            'http://cdn.amazeui.org/amazeui/2.4.2/css/amazeui.css',
-            'http://cdn.amazeui.org/amazeui/2.4.2/css/amazeui.min.css',
-            'css/game/common.css',
+            GlobalUrlService::buildStaticUrl("/amazeui/css/amazeui.css"),
+            GlobalUrlService::buildStaticUrl("/amazeui/css/amazeui.min.css"),
+            'css/wap/common.css',
         ];
         $this->js = [
             'js/jquery/jquery.min.js',
-            'http://cdn.amazeui.org/amazeui/2.4.2/js/amazeui.js',
-            'http://cdn.amazeui.org/amazeui/2.4.2/js/amazeui.min.js',
-            'http://cdn.amazeui.org/amazeui/2.4.2/js/amazeui.ie8polyfill.js',
-            'http://cdn.amazeui.org/amazeui/2.4.2/js/amazeui.ie8polyfill.min.js',
-            'http://cdn.amazeui.org/amazeui/2.4.2/js/amazeui.widgets.helper.js',
-            'http://cdn.amazeui.org/amazeui/2.4.2/js/amazeui.widgets.helper.min.js',
-            'js/game/common.js'
+            GlobalUrlService::buildStaticUrl("/amazeui/js/amazeui.js"),
+            GlobalUrlService::buildStaticUrl("/amazeui/js/amazeui.min.js"),
+            GlobalUrlService::buildStaticUrl("/amazeui/js/amazeui.ie8polyfill.js"),
+            GlobalUrlService::buildStaticUrl("/amazeui/js/amazeui.ie8polyfill.min.js"),
+            GlobalUrlService::buildStaticUrl("/amazeui/js/amazeui.widgets.helper.js"),
+            GlobalUrlService::buildStaticUrl("/amazeui/js/amazeui.widgets.helper.min.js"),
+            'js/wap/common.js'
         ];
         parent::registerAssetFiles($view);
     }
