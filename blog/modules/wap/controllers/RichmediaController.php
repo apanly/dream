328b83acf183b44bc71f9b7b6478a5cb27e62d4e
vincent <apanly@163.com>
2015-11-13 00:07:29 +0800

    vincent -- H5页面也放到cdn中

--- blog/modules/wap/controllers/RichmediaController.php
+++ blog/modules/wap/controllers/RichmediaController.php
@@ -5,6 +5,7 @@ namespace blog\modules\wap\controllers;
 use blog\modules\wap\controllers\common\BaseController;
 use common\components\DataHelper;
 use common\models\posts\RichMedia;
+use common\service\GlobalUrlService;
 use Yii;
 
 class RichmediaController extends BaseController
@@ -19,7 +20,7 @@ class RichmediaController extends BaseController
         $this->setTitle("富媒体--郭大帅哥的生活记录");
         return $this->render("index", [
             "media_list_html" => $this->buildItem($data),
-            "has_next"        => (count($data) < $this->page_size) ? false : true,
+            "has_next" => (count($data) < $this->page_size) ? false : true,
         ]);
     }
 
@@ -36,10 +37,10 @@ class RichmediaController extends BaseController
 
     private function search($params = [])
     {
-        $p      = isset($params['p']) ? $params['p'] : 1;
+        $p = isset($params['p']) ? $params['p'] : 1;
         $offset = ($p - 1) * $this->page_size;
 
-        $query           = RichMedia::find()->where(['status' => 1, 'type' => 'image']);
+        $query = RichMedia::find()->where(['status' => 1, 'type' => 'image']);
         $rich_media_list = $query->orderBy("id desc")
             ->offset($offset)
             ->limit($this->page_size)
@@ -50,10 +51,13 @@ class RichmediaController extends BaseController
         if ($rich_media_list) {
             $domains = Yii::$app->params['domains'];
             foreach ($rich_media_list as $_rich_info) {
+                $tmp_small_pic_url = GlobalUrlService::buildPic1Static($_rich_info['src_url'],['h' => 200]);
+                $tmp_big_pic_url = GlobalUrlService::buildPic1Static($_rich_info['src_url'],['w' => 600]);
                 $data[] = [
-                    'id'        => $_rich_info['id'],
-                    'type'      => $_rich_info['type'],
-                    'src_url'   => $domains['pic1'] . $_rich_info['src_url'],
+                    'id' => $_rich_info['id'],
+                    'type' => $_rich_info['type'],
+                    'small_src_url'   => $tmp_small_pic_url,
+                    'big_src_url'   => $tmp_big_pic_url,
                     'thumb_url' => $_rich_info['thumb_url'] ? $_rich_info['thumb_url'] : $domains['static'] . "/wx/video_cover.jpg",
                     'address'   => $_rich_info['address']
                 ];
--- blog/modules/wap/views/richmedia/item.php
+++ blog/modules/wap/views/richmedia/item.php
@@ -2,9 +2,8 @@
     <?php foreach ($media_list as $_info): ?>
         <li>
             <div class="am-gallery-item">
-                <a href="<?= $_info['src_url']; ?>?format=/2/600" class="">
-                    <img src="<?= $_info['src_url']; ?>?format=/h/200" alt="远方 有一个地方 那里种有我们的梦想"/>
-
+                <a href="<?= $_info['big_src_url']; ?>">
+                    <img src="<?= $_info['small_src_url']; ?>"/>
                     <h3 class="am-gallery-title">
                         <span class="am-icon-map-marker"></span>
                         <?= $_info["address"] ? $_info["address"] : "囧,无GPS信息"; ?>
