04d3ad79ecd6c8b0469ba57dca76e26b71855da8
vincent <apanly@163.com>
2015-11-13 19:14:58 +0800

    vincent -- 全新采集的自动抓取

--- common/service/DoubanmzService.php
+++ common/service/DoubanmzService.php
@@ -5,6 +5,7 @@ namespace common\service;
 
 
 
+use common\components\UploadService;
 use common\models\games\Doubanmz;
 
 class DoubanmzService {
@@ -15,10 +16,13 @@ class DoubanmzService {
         if( $has_in ){
             return true;
         }
+        $tmp_ret = UploadService::uploadByUrl($url,"","pic2");
+
         $model_doubanmz = new Doubanmz();
         $model_doubanmz->hash_key = $hash_key;
         $model_doubanmz->title = $title;
         $model_doubanmz->src_url = $url;
+        $model_doubanmz->image_url = $tmp_ret?$tmp_ret['uri']:'';
         $model_doubanmz->status = 1;
         $model_doubanmz->created_time = date("Y-m-d H:i:s");
         $model_doubanmz->save(0);
