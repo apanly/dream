84166b25b5f0ed9df957515c1d92e0b47f904122
vincent <apanly@163.com>
2015-11-15 15:57:51 +0800

    vincent -- 加个锚点

--- api/modules/weixin/controllers/MenuController.php
+++ api/modules/weixin/controllers/MenuController.php
@@ -73,7 +73,7 @@ class MenuController extends BaseController{
                         [
                             "type" => "view",
                             "name" => "赞助",
-                            "url"  => "{$domain_m}/my/wechat?from={$from}#contact"
+                            "url"  => "{$domain_m}/my/about?from={$from}#contact"
                         ]
                     ]
                 ]
