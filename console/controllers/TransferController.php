1fecfdd290de17373417db5b26a9e979851e4152
vincent <apanly@163.com>
2015-11-24 14:44:29 +0800

    vincent -- 替换一批，先都是用cdn，后续如果不行，在替换回来吧

--- console/controllers/TransferController.php
+++ console/controllers/TransferController.php
@@ -2,26 +2,32 @@
 
 namespace console\controllers;
 
-
 use common\components\UploadService;
 use common\models\games\Doubanmz;
 use common\models\library\Book;
 use common\models\posts\Posts;
+use common\service\GlobalUrlService;
 
 class TransferController extends  BaseController {
     public function actionReplace(){
         $post_list = Posts::find()->where(['status' => 1])->orderBy("id desc")->all();
         if($post_list){
+            $domain_pic1 = \Yii::$app->params['domains']['pic1'];
             foreach( $post_list as $_post_info ){
                 $this->echoLog("post_id:{$_post_info['id']}");
                 $tmp_content = $_post_info['content'];
                 preg_match_all('/<\s*img\s+[^>]*?src\s*=\s*(\'|\")(.*?)\\1[^>]*?\/?\s*>/i',$tmp_content,$match_img);
                 if( $match_img && count($match_img) == 3 ){
                     foreach($match_img[2] as $_img_src){
-                        if( stripos($_img_src,\Yii::$app->params['domains']['pic1']) === false ){
+                        if( stripos($_img_src,$domain_pic1 ) === false ){
                             continue;
                         }
-                        $tmp_content = str_replace($_img_src,$full_img_url,$tmp_content);
+                        $tmp_parse = parse_url($_img_src);
+                        if( $tmp_parse && isset( $tmp_parse['path'] ) ){
+                            $tmp_url = GlobalUrlService::buildPic1Static($tmp_parse['path'],['w' => 600]);
+                            $tmp_content = str_replace($_img_src,$tmp_url,$tmp_content);
+                        }
+
                     }
                     $_post_info->content = $tmp_content;
                     $_post_info->update(0);
