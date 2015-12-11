14a1dfe8dbc3ea2a3aff480c536106e0831d061f
vincent <apanly@163.com>
2015-12-11 16:13:26 +0800

    vincent -- 图片放大操作

--- blog/web/js/wap/default/info.js
+++ blog/web/js/wap/default/info.js
@@ -1,7 +1,23 @@
 ;
 var default_info_ops = {
     init: function () {
+        this.eventBind();
         this.adaptVideo();
+    },
+    eventBind: function () {
+        $("article .am-article-bd img").each(function(){
+            $(this).attr("title","点击查看大图");
+            $(this).attr("alt","点击查看大图");
+            var image_url = $(this).attr("src");
+            var target = $('<a class="zoom" href="'+image_url+'" data-lightbox="roadtrip"></a>');
+            $( this).clone(true).appendTo(target);
+            target.insertBefore(  $(this) );
+            $(this).hide();
+        });
+        lightbox.option({
+            'resizeDuration': 200,
+            'wrapAround': true
+        })
     },
     adaptVideo: function () {
         var width = $(window).width();
