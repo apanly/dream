e7188e84557b06c9a9e4d9d17d1769b293726aed
vincent <apanly@163.com>
2015-12-26 23:16:59 +0800

    vincent -- 手机端也修改掉

--- blog/web/js/wap/default/info.js
+++ blog/web/js/wap/default/info.js
@@ -3,22 +3,11 @@ var default_info_ops = {
     init: function () {
         this.eventBind();
         this.adaptVideo();
+        this.adaptImage();
         this.ifram_bug();//手机端专门修复的
     },
     eventBind: function () {
-        $("article .am-article-bd img").each(function(){
-            $(this).attr("title","点击查看大图");
-            $(this).attr("alt","点击查看大图");
-            var image_url = $(this).attr("src");
-            var target = $('<a class="zoom" href="'+image_url+'" data-lightbox="roadtrip"></a>');
-            $( this).clone(true).appendTo(target);
-            target.insertBefore(  $(this) );
-            $(this).hide();
-        });
-        lightbox.option({
-            'resizeDuration': 200,
-            'wrapAround': true
-        })
+
     },
     adaptVideo: function () {
         var width = $(window).width();
@@ -37,6 +26,37 @@ var default_info_ops = {
                 $(this).hide();
             }
         });
+    },
+    adaptImage:function(){
+        var windowWidth = $(window).width();
+        var dpi = window.devicePixelRatio;
+        var width = windowWidth;
+        if(dpi){
+            width = windowWidth * dpi;
+        }
+        var picwidth = this.calPicWidth(width);
+        if( picwidth > 1024 ){
+            picwidth = 1024;
+        }
+
+        $("article .am-article-bd img").each(function(){
+            $(this).attr("title","点击查看大图");
+            $(this).attr("alt","点击查看大图");
+            var image_url = $(this).attr("src");
+            image_url = image_url.replace(/\/w\/\d+/,"/w/" + picwidth);
+            var target = $('<a class="zoom" href="'+image_url+'" data-lightbox="roadtrip"></a>');
+            $( this).clone(true).appendTo(target);
+            target.insertBefore(  $(this) );
+            $(this).hide();
+        });
+        lightbox.option({
+            'resizeDuration': 200,
+            'wrapAround': true
+        })
+    },
+    calPicWidth:function(width){
+        var tmp_int = Math.ceil(width/50);
+        return tmp_int*50;
     },
     ifram_bug:function(){
         var width = $(document).width() - 30;
