6f488718efb1709aed1dfa9ae73471c3472cf331
vincent <apanly@163.com>
2015-12-26 23:12:34 +0800

    vincent -- 上线看看了

--- blog/web/js/web/default/info.js
+++ blog/web/js/web/default/info.js
@@ -3,22 +3,11 @@ var default_info_ops = {
     init: function () {
         this.eventBind();
         this.adaptVideo();
+        this.adaptImage();
         this.syntaxHighlighter();
     },
     eventBind: function () {
-        $("article .post-content img").each(function(){
-            $(this).attr("title","点击查看大图");
-            $(this).attr("alt","点击查看大图");
-            var image_url = $(this).attr("src");
-            var target = $('<a class="zoom" href="'+image_url+'" data-lightbox="roadtrip"></a>');
-            $( this).clone(true).appendTo(target);
-            target.insertBefore(  $(this) );
-            $(this).hide();
-        });
-        lightbox.option({
-            'resizeDuration': 200,
-            'wrapAround': true
-        })
+
     },
     adaptVideo: function () {
         var width = $(window).width();
@@ -30,6 +19,37 @@ var default_info_ops = {
                 $(this).attr("height", height);
             }
         });
+    },
+    adaptImage:function(){
+        var windowWidth = $(window).width();
+        var dpi = window.devicePixelRatio;
+        var width = windowWidth;
+        if(dpi){
+            width = windowWidth * dpi;
+        }
+        var picwidth = this.calPicWidth(width);
+        if( picwidth > 1024 ){
+            picwidth = 1024;
+        }
+
+        $("article .post-content img").each(function(){
+            $(this).attr("title","点击查看大图");
+            $(this).attr("alt","点击查看大图");
+            var image_url = $(this).attr("src");
+            image_url = image_url.replace(/w\/\d+/,"w/" + picwidth);
+            var target = $('<a class="zoom" href="'+image_url+'" data-lightbox="roadtrip"></a>');
+            $( this).clone(true).appendTo(target);
+            target.insertBefore(  $(this) );
+            $(this).hide();
+        });
+        lightbox.option({
+            'resizeDuration': 200,
+            'wrapAround': true
+        })
+    },
+    calPicWidth:function(width){
+        var tmp_int = Math.ceil(width/50);
+        return tmp_int*50;
     },
     syntaxHighlighter:function(){
         $("pre").each(function(){
