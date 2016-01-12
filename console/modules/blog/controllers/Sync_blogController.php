7a9301c662e1271abea7ebbb3313df69c55668ef
vincent <apanly@163.com>
2016-01-12 21:56:37 +0800

    vincent -- 都要重新更新了

--- console/modules/blog/controllers/Sync_blogController.php
+++ console/modules/blog/controllers/Sync_blogController.php
@@ -34,4 +34,20 @@ class Sync_blogController extends Blog{
         }
         return $this->echoLog(" Done ");
     }
+
+    public function actionInit(){
+        $date_now = date("Y-m-d H:i:s");
+        $posts_list = Posts::find()->where(['status' => 1])->orderBy("id asc")->all();
+        foreach( $posts_list as $_item ){
+            foreach( SyncBlogService::$type_mapping as $_type => $_f ){
+                    $model_blog_sync_queue = new BlogSyncQueue();
+                    $model_blog_sync_queue->blog_id = $_item['id'];
+                    $model_blog_sync_queue->type = $_type;
+                    $model_blog_sync_queue->updated_time = $date_now;
+                    $model_blog_sync_queue->created_time = $date_now;
+                    $model_blog_sync_queue->status = -1;
+                    $model_blog_sync_queue->save(0);
+            }
+        }
+     }
 }
