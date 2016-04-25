09bb9895d7c7f1a853289b00dca6b09d7d6094f9
vincent <apanly@163.com>
2016-04-25 09:51:58 +0800

    Vincent -- 修改下标题

--- common/config/params.php
+++ common/config/params.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'seo' => [
-        'description' => '郭大帅哥的梦博客，一个有梦的php开发工程师',
-        'keywords' => '郭大帅哥的梦博客，一个有梦的php开发工程师'
+        'description' => '编程浪子的梦博客，一个有梦的php开发工程师',
+        'keywords' => '编程浪子的梦博客，一个有梦的php开发工程师'
     ],
     'Copyright' => "<a href='http://m.vincentguo.cn'>移动版</a>
-    <br/>Copyright © 2015.郭大帅哥 All rights reserved | PHP，Python 工程师"
+    <br/>Copyright © 2015.编程浪子 All rights reserved | PHP，Python 工程师"
 ];
