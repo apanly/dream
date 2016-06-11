b22a81c52bbd8886c9417b98d66e60163e07b52f
vincent <apanly@163.com>
2016-06-11 14:33:56 +0800

    vincent  -- 需要隐藏

--- blog/views/public/footer.php
+++ blog/views/public/footer.php
@@ -68,7 +68,7 @@ $post_latest = array_slice($data['post_latest'],0,5);
 </div>
 
 <a href="javascript:void(0);" id="back-to-top"><i class="fa fa-angle-up"></i></a>
-<div class="hidden_wrap">
+<div class="hidden_wrap" style="display: none;">
     <input type="hidden" id="access_domain" value="<?=\Yii::$app->params['domains']['blog'];?>">
     <input type="hidden" id="domain_static" value="<?=GlobalUrlService::buildStaticUrl("/");?>">
     <ul class="friend_links">
