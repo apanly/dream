28cbc92aa93e03d18ffdcd0957d321b6f7a128b8
vincent <apanly@163.com>
2016-06-15 23:55:59 +0800

    vincent -- 修改下这个页面

--- blog/modules/wap/views/wechat_wall/sign.php
+++ blog/modules/wap/views/wechat_wall/sign.php
@@ -12,7 +12,7 @@ StaticService::includeAppJsStatic("/js/wap/wechat_wall/sign.js",\blog\assets\Mar
 </div>
 <div class="checkin_info">
     <p class="t1">恭喜您上墙成功！</p>
-    <p class="t2">关闭本页面直接发送消息即可参与互动哦！</p>
+    <p class="t2">关闭本页面直接发送“<span style="font-weight: bold;color: #ff0000;font-size: large;">#消息内容</span>”即可参与互动哦！</p>
 </div>
 <a href="javascript:void(0)" id="close_checkin_window" class="button">关闭本页面</a>
 
