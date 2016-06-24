6a9bc74c9ca45560007153489c6da0aa55ddbc5f
vincent <apanly@163.com>
2016-06-24 16:53:42 +0800

    vincent -- 加入key

--- environments/dev/admin/config/params-local.php
+++ environments/dev/admin/config/params-local.php
@@ -1,3 +1,7 @@
 <?php
 return [
+	'account' => [
+		'public_key_path' => '/home/www/yii_tools/test/account_public_key.pem',
+		'private_key_path' => '/home/www/yii_tools/test/account_private_key.pem'
+	]
 ];
--- environments/prod/admin/config/params-local.php
+++ environments/prod/admin/config/params-local.php
@@ -1,3 +1,7 @@
 <?php
 return [
+	'account' => [
+		'public_key_path' => '/data/www/ssl_keys/account_public_key.pem',
+		'private_key_path' => '/data/www/ssl_keys/account_private_key.pem'
+	]
 ];
