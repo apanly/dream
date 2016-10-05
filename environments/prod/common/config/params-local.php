09d06dbb49e6e54cf912108a202f42981cbce286
vincent <apanly@163.com>
2016-10-05 16:04:53 +0800

    vincent  -- 准备打个android 包

--- environments/prod/common/config/params-local.php
+++ environments/prod/common/config/params-local.php
@@ -10,8 +10,9 @@ return [
         "cdn_static" => "http://cdn.static.yunetidc.com",
         "pic1" => "http://pic1.vincentguo.cn",
         "cdn_pic1" => "http://cdn.pic1.yunetidc.com",
-        "pic2" => "http://pic2.vincentguo.cn",
+        "pic2" => "http://cdn.pic2.yunetidc.com",
         "cdn_pic2" => "http://cdn.pic2.yunetidc.com",
+        "cdn_pic2_https" => "https://oeil0b8s3.qnssl.com",
         "pic3" => "http://pic3.vincentguo.cn",
         "cdn_pic3" => "http://cdn.pic3.yunetidc.com",
         "m"  => "http://m.vincentguo.cn",
@@ -42,6 +43,10 @@ return [
         "pic1" => "/data/www/pic1/",
         "pic2" => "/data/www/pic2/",
         "pic3" => "/data/www/pic3/",
+        'qiniu_config' => [
+            'ak' => 'xxx',
+            'sk' => 'xxx'
+        ]
     ],
     'switch' => [
         "cdn" => [
@@ -50,5 +55,8 @@ return [
             "pic2" => false,
             "pic3" => false
         ]
+    ],
+    'report' => [
+        'dir_path' => '/tmp/report/'
     ]
 ];
