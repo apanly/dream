be9c03c1fe5c7fa394c887bb8327316797f59cb5
vincent <apanly@163.com>
2016-10-16 22:52:20 +0800

    vincent -- uuid统计ok，跑数据去

--- console/modules/report/controllers/Stat_dailyController.php
+++ console/modules/report/controllers/Stat_dailyController.php
@@ -4,6 +4,7 @@ namespace console\modules\report\controllers;
 
 use common\models\applog\AccessLogs;
 use common\models\applog\StatDailyAccessSource;
+use common\models\applog\StatDailyUuid;
 
 class Stat_dailyController extends Stat_utilController{
 	/*
@@ -22,6 +23,7 @@ class Stat_dailyController extends Stat_utilController{
 			->select([ 'source','COUNT(*) as total_number' ])
 			->where([ '>=','created_time',$time_from ])
 			->andWhere([ '<=','created_time',$time_to ])
+			->groupBy("source")
 			->asArray()
 			->all();
 
@@ -49,5 +51,53 @@ class Stat_dailyController extends Stat_utilController{
 			$tmp_model_info->save(0);
 		}
 
+		return $this->echoLog("it's over~~");
+	}
+
+	/*
+	 * php yii report/stat_daily/uuid
+	 * */
+	public function actionUuid( $date = '' ){
+		$date = $date?$date:date("Y-m-d");
+		if( !preg_match("/^\d{4}-\d{2}-\d{2}$/",$date) ){
+			return $this->echoLog("param date format error,2016-04-28");
+		}
+
+		$time_from = date("Y-m-d 00:00:00",strtotime( $date ) );
+		$time_to = date("Y-m-d 23:59:59",strtotime( $date ) );
+
+		$list = AccessLogs::find()
+			->select([ 'uuid','COUNT(*) as total_number' ])
+			->where([ '>=','created_time',$time_from ])
+			->andWhere([ '<=','created_time',$time_to ])
+			->groupBy("uuid")
+			->asArray()
+			->all();
+
+		if( !$list ){
+			return $this->echoLog('no data to handle~~');
+		}
+
+		$date_int = date("Ymd",strtotime( $date ) );
+		$date_now = date("Y-m-d H:i:s");
+		foreach( $list as $_item ){
+			$tmp_uuid = $_item['source'];
+			$tmp_total_number = $_item['total_number'];
+			$tmp_info = StatDailyUuid::find()
+				->where([ 'date' => $date_int,'uuid' => $tmp_uuid ])
+				->one();
+			if( $tmp_info ){
+				$tmp_model_info = $tmp_info;
+			}else{
+				$tmp_model_info = new StatDailyUuid();
+				$tmp_model_info->date = $date_int;
+				$tmp_model_info->uuid = $tmp_uuid;
+			}
+			$tmp_model_info->total_number = $tmp_total_number;
+			$tmp_model_info->created_time = $date_now;
+			$tmp_model_info->save(0);
+		}
+
+		return $this->echoLog("it's over~~");
 	}
 }
