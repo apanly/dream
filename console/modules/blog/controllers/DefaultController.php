072d5fc484bf3e3087a55523b1844729615b2fe7
vincent <apanly@163.com>
2016-11-09 17:52:11 +0800

    vincent -- 知道错误在哪里了

--- console/modules/blog/controllers/DefaultController.php
+++ console/modules/blog/controllers/DefaultController.php
@@ -3,52 +3,11 @@
 namespace console\modules\blog\controllers;
 
 use common\models\applog\AdCspReport;
+use common\service\SyncBlogService;
 use console\modules\blog\Blog;
 
 
 class DefaultController extends Blog{
     public function actionIndex(){
-
     }
-
-    public function actionCsp(){
-		$list = AdCspReport::find()->orderBy([ 'id' => SORT_ASC ])->all();
-		$data = [];
-		foreach( $list as $_item ){
-			$tmp_content = @json_decode( $_item['report_content'],true );
-
-			if( !$tmp_content || !isset($tmp_content['csp-report']) || !isset( $tmp_content['csp-report']['blocked-uri'] ) ){
-				continue;
-			}
-
-			$tmp_blocked_uri = $tmp_content['csp-report']['blocked-uri'];
-			$tmp_blocked_uri = parse_url( $tmp_blocked_uri );
-			if( !isset($tmp_blocked_uri['host'])  ){
-				continue;
-			}
-			$tmp_port = isset( $tmp_blocked_uri['port'] )?$tmp_blocked_uri['port']:'';
-			$tmp_blocked_uri = $tmp_blocked_uri['host'];
-			if( $tmp_port ){
-				$tmp_blocked_uri .= ":{$tmp_port}";
-			}
-
-			$_item->blocked_uri = $tmp_blocked_uri;
-			if( isset( $tmp_content['csp-report']['source-file'] ) ){
-				$_item->source_file = $tmp_content['csp-report']['source-file'];
-			}
-			$_item->update(0);
-
-
-
-			if( !isset( $data[ $tmp_blocked_uri ] ) ){
-				$data[ $tmp_blocked_uri  ] = 0;
-			}
-			$data[ $tmp_blocked_uri  ]++;
-		}
-		asort( $data,SORT_NUMERIC );
-		unset( $list );
-		foreach( $data as $_domain => $_counter ){
-			echo $_counter."	".$_domain."\r\n";
-		}
-	}
 }
