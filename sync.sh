f8f348eb73444544bb23bd59f34b3af229b4f910
vincent <apanly@163.com>
2016-11-18 23:54:38 +0800

    vincent -- 脚本同时同步到两个平台

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git fetch --all && git rebase origin/master && git push origin master:master
-git fetch --all && git rebase oschina/master && git push oschina master:master
+git fetch origin && git rebase origin/master && git push origin master:master
+git fetch oschina && git rebase oschina/master && git push oschina master:master
 echo 'push over'
