73082af4bc8bfa4fecf14ea2545d6f49e2b35714
vincent <apanly@163.com>
2016-11-18 23:53:25 +0800

    vincent -- 发布

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git fetch --all && git rebase origin/master && git push origin master:master
-git fetch --all && git oschina origin/master && git push oschina master:master
+git fetch --all && git rebase oschina/master && git push oschina master:master
 echo 'push over'
