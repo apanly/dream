b4bdbacaa8aad9a4c1678de9a94c77fad84a2124
vincent <apanly@163.com>
2016-12-16 10:07:14 +0800

    vincent  -- 两个库都保存起来

--- console/controllers/BackupController.php
+++ console/controllers/BackupController.php
@@ -13,18 +13,21 @@ class BackupController extends  BaseController {
      */
     public function actionMysql(){
 
-        $filename = date("Y-m-d").".sql";
-        $config_mysql = \Yii::$app->components['blog'];
-        $mysql_user = $config_mysql['username'];
-        $mysql_passwd = $config_mysql['password'];
-        $db_name = "dream_blog dream_log";
+		$config_mysql = \Yii::$app->components['blog'];
+		$mysql_user = $config_mysql['username'];
+		$mysql_passwd = $config_mysql['password'];
+		$db_names = [ "dream_blog","dream_log" ];
+		foreach( $db_names as $db_name){
+			$filename = $db_name."_".date("Y-m-d").".sql";
+			$command = "cd /data/www/backup/ && /usr/bin/mysqldump -u{$mysql_user}".($mysql_passwd?"  -p{$mysql_passwd}":" ").  " {$db_name} --skip-lock-tables > {$filename}";
+			exec($command);
+			$this->echoLog("backp mysql:".$command);
 
-        $command = "cd /data/www/backup/ && /usr/bin/mysqldump -u{$mysql_user}".($mysql_passwd?"  -p{$mysql_passwd}":" ").  " {$db_name} --skip-lock-tables > {$filename}";
-        exec($command);
-        $this->echoLog("backp mysql:".$command);
+			$command = "cd /data/www/backup/ && tar -zcf {$filename}.tar.gz {$filename} && rm {$filename}";
+			exec($command);
+			$this->echoLog("tar backup_mysql:".$command);
+		}
 
-        $command = "cd /data/www/backup/ && tar -zcf {$filename}.tar.gz {$filename} && rm {$filename}";
-        exec($command);
-        $this->echoLog("tar backup_mysql:".$command);
+
     }
 } 
