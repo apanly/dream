f50cb0efd533cbf62bf85e422ec58a66b38b4f58
vincent <apanly@163.com>
2017-01-19 09:23:05 +0800

    vincent --  备案号不对了

--- common/config/params.php
+++ common/config/params.php
@@ -10,5 +10,5 @@ return [
         'keywords' => 'php教程，python教程，php开发，编程浪子的博客'
     ],
     'Copyright' => "<a href='http://m.vincentguo.cn'>移动版</a>
-    <br/>Copyright © 2016.编程浪子 All rights reserved | 备案号：青ICP备13000097号-81"
+    <br/>Copyright © 2016.编程浪子 All rights reserved | 备案号：沪ICP备13003569号-2"
 ];
