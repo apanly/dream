dfe2978af91cc83bfa46d49905f6b0d2acde8194
vincent <guowei@mogoroom.com>
2017-09-28 09:58:52 +0800

    vincent -- 支付

--- blog/web/js/wap/demo/mogo_pay.js
+++ blog/web/js/wap/demo/mogo_pay.js
@@ -44,7 +44,7 @@ var demo_mogopay_ops = {
                     // } );
                     WeixinJSBridge.invoke(
                         'getBrandWCPayRequest', {
-                            "appId": data.appid,     //公众号名称，由商户传入
+                            "appId": data.appId,     //公众号名称，由商户传入
                             "timeStamp":data.timeStamp,         //时间戳，自1970年以来的秒数
                             "nonceStr":data.nonceStr, //随机串
                             "package":data.package,
@@ -55,17 +55,14 @@ var demo_mogopay_ops = {
                             //// 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
                             if(res.err_msg == "get_brand_wcpay_request:ok" ) {
                                 alert("支付成功~~");
-                                window.location.href = window.location.href;
                             }
 
                             if(res.err_msg == "get_brand_wcpay_request:cancel" ) {
                                 alert("支付取消~~");
-                                window.location.href = window.location.href;
                             }
 
                             if(res.err_msg == "get_brand_wcpay_request:fail" ) {
                                 alert("支付失败~~");
-                                window.location.href = window.location.href;
                             }
                         }
                     );
@@ -117,13 +114,11 @@ var demo_mogopay_ops = {
     }
 };
 
+function onBridgeReady(){
+    demo_mogopay_ops.init();
+}
 
 $(document).ready( function(){
-    demo_mogopay_ops.init();
-
-    function onBridgeReady(){
-
-    }
     if (typeof WeixinJSBridge == "undefined"){
         if( document.addEventListener ){
             document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
