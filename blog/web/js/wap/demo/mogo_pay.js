33ad853230e37a042745993b3cd447379251d435
vincent <guowei@mogoroom.com>
2017-09-28 09:55:25 +0800

    vincent -- 看看行不行了

--- blog/web/js/wap/demo/mogo_pay.js
+++ blog/web/js/wap/demo/mogo_pay.js
@@ -39,9 +39,36 @@ var demo_mogopay_ops = {
                             alert("支付失败~~");
                         }
                     };
-                    wx.ready( function(){
-                        wx.chooseWXPay( json_data );
-                    } );
+                    // wx.ready( function(){
+                    //     wx.chooseWXPay( json_data );
+                    // } );
+                    WeixinJSBridge.invoke(
+                        'getBrandWCPayRequest', {
+                            "appId": data.appid,     //公众号名称，由商户传入
+                            "timeStamp":data.timeStamp,         //时间戳，自1970年以来的秒数
+                            "nonceStr":data.nonceStr, //随机串
+                            "package":data.package,
+                            "signType":data.signType,         //微信签名方式：
+                            "paySign":data.paySign //微信签名
+                        },
+                        function( res ){
+                            //// 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+                            if(res.err_msg == "get_brand_wcpay_request:ok" ) {
+                                alert("支付成功~~");
+                                window.location.href = window.location.href;
+                            }
+
+                            if(res.err_msg == "get_brand_wcpay_request:cancel" ) {
+                                alert("支付取消~~");
+                                window.location.href = window.location.href;
+                            }
+
+                            if(res.err_msg == "get_brand_wcpay_request:fail" ) {
+                                alert("支付失败~~");
+                                window.location.href = window.location.href;
+                            }
+                        }
+                    );
                 }
             });
         });
@@ -87,10 +114,24 @@ var demo_mogopay_ops = {
                 }
             }
         });
-    },
+    }
 };
 
 
 $(document).ready( function(){
     demo_mogopay_ops.init();
+
+    function onBridgeReady(){
+
+    }
+    if (typeof WeixinJSBridge == "undefined"){
+        if( document.addEventListener ){
+            document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+        }else if (document.attachEvent){
+            document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+            document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+        }
+    }else{
+        onBridgeReady();
+    }
 } );
