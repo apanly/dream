bb1d108bc3ee15fc530da0849a89ebe81a81d0be
vincent <guowei@mogoroom.com>
2019-02-12 15:20:01 +0800

    vincent -- 只要cnzz统计

--- blog/views/public/stat.php
+++ blog/views/public/stat.php
@@ -1,13 +1,4 @@
 <div style="display:none">
-    <!-- cnzz stats -->
-    <script type="text/javascript">
-        var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");
-        var cnzz_s_tag = document.createElement('script');
-        cnzz_s_tag.type = 'text/javascript';
-        cnzz_s_tag.async = true;
-        cnzz_s_tag.charset = 'utf-8';
-        cnzz_s_tag.src = cnzz_protocol + 's95.cnzz.com/z_stat.php?id=1261156679&async=1';
-        var root_s = document.getElementsByTagName('script')[0];
-        root_s.parentNode.insertBefore(cnzz_s_tag, root_s);
-    </script>
+    <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+        document.write(unescape("%3Cspan id='cnzz_stat_icon_1261156679'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1261156679' type='text/javascript'%3E%3C/script%3E"));</script>
 </div>
