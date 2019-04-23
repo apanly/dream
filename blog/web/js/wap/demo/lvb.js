7f37e782cf3b9177cfb2ae08c22aa6808a15384b
vincent <guowei@mogoroom.com>
2019-04-23 22:16:30 +0800

    vincent -- 直播

--- blog/web/js/wap/demo/lvb.js
+++ blog/web/js/wap/demo/lvb.js
@@ -9,7 +9,7 @@ var demo_lvb_ops = {
             "m3u8": "http://lvb.54php.cn/live/54php.m3u8",
             "flv": "http://lvb.54php.cn/live/54php.flv", //增加了一个flv的播放地址，用于PC平台的播放 请替换成实际可用的播放地址
             "autoplay" : true,      //iOS下safari浏览器，以及大部分移动端浏览器是不开放视频自动播放这个能力的
-            "coverpic" : "http://www.test.com/myimage.jpg",
+            "coverpic" : "http://cdn.pic1.54php.cn/20190423/48b140b0882ee336edfe3ad3d98dd93a.jpg",
             "width" :  '480',//视频的显示宽度，请尽量使用视频分辨率宽度
             "height" : '320'//视频的显示高度，请尽量使用视频分辨率高度
         });
